21bd2c04a7ee79c01a8c5f6a82619377d9809617
Nifemi Leye <147550833+nnifemi@users.noreply.github.com>
2023-12-06 15:28:41 -0600

    Update index.js

--- assets/script/index.js
+++ assets/script/index.js
@@ -10,14 +10,17 @@ const settingsButton = document.getElementById('settings');
 const body = document.body;
 const mainContent = document.getElementById('main-content');
 
+// Function to display the initial cookie information dialog
 function showCookieInformationDialog() {
+    // Create Accept button
     const acceptButton = document.createElement('button');
     mainContent.classList.add('blurry-background');
     acceptButton.textContent = 'Accept';
     acceptButton.className = 'accept-all';
     acceptButton.onclick = acceptAllCookies;
 
-    const settingsBtn = document.createElement('button'); // Changed variable name to settingsBtn
+    // Create Settings button
+    const settingsBtn = document.createElement('button');
     settingsBtn.textContent = 'Settings';
     settingsBtn.className = 'settings';
     settingsBtn.onclick = showCookieSettingsDialog;
@@ -28,13 +31,16 @@ function showCookieInformationDialog() {
     cookieDialog.appendChild(acceptButton);
     cookieDialog.appendChild(settingsBtn);
 
+    // Display the cookie information dialog
     cookieDialog.style.display = 'block';
 }
 
+// Function to display the cookie settings dialog
 function showCookieSettingsDialog() {
     // Hide cookieDialog
     cookieDialog.style.display = 'none';
 
+    // Generate HTML for cookie settings
     cookieSettings.innerHTML = `
         <p>Settings</p>
         <div class="slider-group">
@@ -64,16 +70,18 @@ function showCookieSettingsDialog() {
         saveSettingsButton.addEventListener('click', saveCookieSettings);
     }
 
+    // Display the cookie settings dialog
     cookieSettings.style.display = 'block';
 }
 
+// Function to handle accepting all cookies
 function acceptAllCookies() {
     createCookie('allCookiesAccepted', 'true');
     console.log('Cookies have been accepted.');
     hideCookieDialogs();
 }
 
-
+// Function to initialize cookie settings
 function initializeCookieSettings() {
     const browserCheckbox = document.getElementById('browserCheckbox');
     const osCheckbox = document.getElementById('osCheckbox');
@@ -81,9 +89,12 @@ function initializeCookieSettings() {
     const screenHeightCheckbox = document.getElementById('screenHeightCheckbox');
     const screenWidthCheckbox = document.getElementById('screenWidthCheckbox');
 
+    // Check if checkboxes are found
     if (browserCheckbox && osCheckbox && screenCheckbox && screenHeightCheckbox && screenWidthCheckbox) {
+        // Check if all cookies are already accepted
         const allCookiesAccepted = getCookie('allCookiesAccepted') === 'true';
 
+        // Hide dialogs if all cookies are accepted
         if (allCookiesAccepted) {
             hideCookieDialogs();
         }
@@ -94,14 +105,15 @@ function initializeCookieSettings() {
             saveSettingsButton.addEventListener('click', displaySettingsInfo);
         }
     } else {
+        // Log an error if one or more checkboxes are not found
         console.error('One or more checkboxes not found.');
     }
 }
 
-
 // Call the function when the window has loaded
 window.addEventListener('load', initializeCookieSettings);
 
+// Function to save cookie settings
 function saveCookieSettings() {
     const browserCheckbox = document.getElementById('browserCheckbox').checked;
     const osCheckbox = document.getElementById('osCheckbox').checked;
@@ -137,27 +149,31 @@ function saveCookieSettings() {
     cookieSettings.style.display = 'none';
 }
 
-
+// Function to get the screen width
 function getScreenWidth() {
     return window.innerWidth;
 }
 
+// Function to get the screen height
 function getScreenHeight() {
     return window.innerHeight;
 }
 
+// Function to hide cookie dialogs
 function hideCookieDialogs() {
     mainContent.classList.remove('blurry-background');
     cookieDialog.style.display = 'none';
     cookieSettings.style.display = 'none';
 }
 
+// Function to create a cookie
 function createCookie(name, value) {
     const expires = new Date();
     expires.setSeconds(expires.getSeconds() + 15); // Cookies live for 15 seconds
     document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/;SameSite=None;Secure`;
 }
 
+// Function to get a cookie by name
 function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -165,19 +181,23 @@ function getCookie(name) {
     return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
+// Function to display the cookie information dialog with a delay
 function showCookieInformationDialogWithDelay() {
     setTimeout(() => {
         showCookieInformationDialog();
     }, 1000);
 }
 
+// Check if cookies are enabled and if the user has accepted cookies previously
 const cookiesEnabled = navigator.cookieEnabled;
 const storedCookies = getCookie('allCookiesAccepted');
 
+// Show the cookie information dialog with a delay if cookies are not enabled or not accepted
 if (!cookiesEnabled || storedCookies === undefined) {
     showCookieInformationDialogWithDelay();
 }
 
+// Function to get the browser name
 function getBrowserName() {
     const userAgent = navigator.userAgent;
 
@@ -198,9 +218,11 @@ function getBrowserName() {
     }
 }
 
+// Display the browser name on the page
 const browserName = getBrowserName();
 document.getElementById('Browser').innerHTML = (`Browser : ${browserName}.`);
 
+// Function to get the operating system
 function getOperatingSystem() {
     const platform = navigator.userAgent;
 
@@ -219,6 +241,7 @@ function getOperatingSystem() {
     }
 }
 
+// Function to set window dimensions on page load and resize
 function setWindowDimensions() {
     const pageW = document.getElementById('pageW');
     const pageH = document.getElementById('pageH');
@@ -229,6 +252,7 @@ function setWindowDimensions() {
     }
 }
 
+// Add event listeners for window load and resize to set window dimensions
 window.addEventListener('load', () => {
     setWindowDimensions();
     checkOrientation();
@@ -239,8 +263,8 @@ window.addEventListener('resize', () => {
     checkOrientation();
 });
 
+// Function to get the page orientation and update the pageO element
 const pageO = document.getElementById('pageO');
-
 function checkOrientation() {
     if (pageO) {
         if (window.innerHeight > window.innerWidth) {
@@ -251,14 +275,17 @@ function checkOrientation() {
     }
 }
 
+// Add event listener for window resize to check and update orientation
 window.addEventListener('resize', () => {
     checkOrientation();
 });
 
+// Call checkOrientation on window load
 window.addEventListener('load', () => {
     checkOrientation();
 });
 
+// Function to display settings information
 function displaySettingsInfo() {
     const infoContainer = document.getElementById('settingsInfo');
     const browserCheckbox = document.getElementById('browserCheckbox').checked;
@@ -299,7 +326,6 @@ function displaySettingsInfo() {
     // Log information to the console
     console.log(info);
 }
-
 
 // Add event listener to display settings information on Save Settings button click
 const saveSettingsButton = document.getElementById('saveSettings');
